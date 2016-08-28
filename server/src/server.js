3aeceb9c15db825dae541f0b55cf50c5366635f6
Marcin Zolkiewski <Marcin_Zolkiewski@epam.com>
2016-08-28 12:37:14 +0200

    finished tutorial

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,12 @@
 import Server from 'socket.io';
 
-export function startServer(store) {
+export default function startServer(store) {
   const io = new Server().attach(8090);
 
   store.subscribe(() => io.emit('state', store.getState().toJS()));
 
   io.on('connection', (socket) => {
+    console.log('client connected');
     socket.emit('state', store.getState().toJS());
     socket.on('action', store.dispatch.bind(store));
   });
